756a8eeb381396ebd2930ace346dca44b143a6e1
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-01-13 23:54:30 +0100

    implemented the homescreen component and redux store

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import { store } from "./store";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
-    <Provider store={}>
-      <View style={styles.container}>
-        <Text>Lets Build Uberclone</Text>
-      </View>
+    <Provider store={store}>
+      <HomeScreen />
     </Provider>
   );
 }
