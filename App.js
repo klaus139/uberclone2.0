3d1a9b4d8c0f799729b228ea469df5e7705844d1
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-01-15 09:48:35 +0100

    the google api key is not correct and i cant get a new one yet, ill continue from 1.54 when i do

--- App.js
+++ App.js
@@ -2,12 +2,39 @@ import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import HomeScreen from "./screens/HomeScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MapScreen from "./screens/MapScreen";
+
+
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{ headerShown: false, onSwipeLeft: () => { navigation.goBack() },
+              gestureEnabled: true, }}
+              
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShown: false, onSwipe: () => { navigation.goBack() },
+              gestureEnabled: true, }}
+            />   
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
+
   );
 }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,42 @@
-import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Image, TextInput } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_APIKEY} from "@env";
 
-const HomeScreen = () => {
+
+
+const HomeScreen = ({navigation}) => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
         <Image
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={{ uri: "https://links.papareact.com/gzs" }}
+        />
+        <GooglePlacesAutocomplete 
+        placeholder="Where from?"
+        styles={{
+            container:{
+                flex: 0,
+            },
+            textInput: {
+                fontSize: 18,
+            },
+        }}
+        enablePoweredByContainer={false}
+        onPress={(data, details = null)=>{
+            console.log(data);
+            console.log(details);
+        }}
+        minLength={2}
+        query={{
+            key:GOOGLE_MAPS_APIKEY,
+            language: 'en',
+        }}
+        nearByPlacesAPI="GooglePlacesSearch"
+        debounce={400}
         />
         <NavOptions />
       </View>
